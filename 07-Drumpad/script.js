f3c4bad92a5f56ac628e94e28cd81e202ef44aa9
Am.D <ameliedelloro@gmail.com>
2021-05-04 15:55:14 +0200

    07.1 added

---
+++ 07-Drumpad/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function () {
+    var samples = [new Audio('assets/L07_task_material_assets_kick.mp3'), new Audio('assets/L07_task_material_assets_snare.mp3'), new Audio('assets/L07_task_material_assets_hihat.mp3'), new Audio('assets/L07_task_material_assets_A.mp3'), new Audio('assets/L07_task_material_assets_C.mp3'), new Audio('assets/L07_task_material_assets_F.mp3'), new Audio('assets/L07_task_material_assets_G.mp3'), new Audio('assets/L07_task_material_assets_laugh-1.mp3'), new Audio('assets/L07_task_material_assets_laugh-2.mp3')];
+    document.querySelector('#kick').addEventListener('click', function () { playSample(samples[0]); });
+    document.querySelector('#snare').addEventListener('click', function () { playSample(samples[1]); });
+    document.querySelector('#hihat').addEventListener('click', function () { playSample(samples[2]); });
+    document.querySelector('#A').addEventListener('click', function () { playSample(samples[3]); });
+    document.querySelector('#C').addEventListener('click', function () { playSample(samples[4]); });
+    document.querySelector('#F').addEventListener('click', function () { playSample(samples[5]); });
+    document.querySelector('#G').addEventListener('click', function () { playSample(samples[6]); });
+    document.querySelector('#laugh1').addEventListener('click', function () { playSample(samples[7]); });
+    document.querySelector('#laugh2').addEventListener('click', function () { playSample(samples[8]); });
+    function playSample(sample) {
+        if (sample === void 0) { sample = new Audio; }
+        sample.play();
+    }
+    ;
+});
+/*
+
+var audio = new Audio('audio_file.mp3');
+audio.play();
+
+*/ 
+//# sourceMappingURL=script.js.map
