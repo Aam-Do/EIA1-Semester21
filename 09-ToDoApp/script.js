e0b808c5dd64d8d215bd853e6797e38b4c3ae947
Am.D <ameliedelloro@gmail.com>
2021-05-20 18:43:53 +0200

    trashcan added

--- 09-ToDoApp/script.js
+++ 09-ToDoApp/script.js
@@ -5,23 +5,30 @@ window.addEventListener("load", function () {
     console.log(allToDos);
     toDoInput.addEventListener("keydown", function (event) {
         if (event.keyCode == 13) {
-            allToDos.push({ checked: false, content: toDoInput.value, number: allToDos.length });
+            var length_1 = allToDos.length;
+            allToDos.push({ checked: false, content: toDoInput.value, toDoId: length_1.toString(), checkmarkId: "check" + length_1.toString(), trashId: "trash" + length_1.toString() });
             console.log(allToDos);
             displayList();
         }
     });
     function displayList() {
         list.innerHTML = "";
-        for (var i = 0; i < allToDos.length; i++) {
+        var _loop_1 = function (i) {
             var todo = allToDos[i];
             var newToDo = document.createElement("li");
             var node = document.createTextNode(todo.content);
             var idToDo = document.createAttribute("id");
             var idChecked = document.createAttribute("id");
+            var idTrash = document.createAttribute("id");
             idToDo.value = i.toString();
             idChecked.value = "check" + i.toString();
+            idTrash.value = "trash" + i.toString();
             var checkbox = document.createElement("i");
             var checked = document.createAttribute("class");
+            var trashIcon = document.createElement("i");
+            var trash = document.createAttribute("class");
+            trash.value = "far fa-trash-alt";
+            trashIcon.setAttributeNode(trash);
             if (allToDos[i].checked == false) {
                 checked.value = "far fa-circle";
                 checkbox.setAttributeNode(checked);
@@ -34,12 +41,32 @@ window.addEventListener("load", function () {
             newToDo.appendChild(checkbox);
             newToDo.appendChild(node);
             newToDo.setAttributeNode(idToDo);
+            newToDo.appendChild(trashIcon);
             list.appendChild(newToDo);
-            // checkbox.addEventListener("click", function (): void {checkToDo(); });
+            checkbox.addEventListener("click", function () { checkToDo(idChecked.value); });
+        };
+        for (var i = 0; i < allToDos.length; i++) {
+            _loop_1(i);
         }
         toDoInput.value = "";
     }
-    // function checkToDo(): void {
-    // }
+    function checkToDo(id) {
+        for (var i = 0; i < allToDos.length; i++) {
+            var todo = allToDos[i];
+            if (todo.checkmarkId == id) {
+                var check = document.querySelector("#" + id);
+                if (todo.checked == false) {
+                    check.setAttribute("class", "fas fa-check-circle");
+                    todo.checked = true;
+                    console.log("heck");
+                }
+                else {
+                    check.setAttribute("class", "far fa-circle");
+                    todo.checked = false;
+                    console.log("yeah");
+                }
+            }
+        }
+    }
 });
 //# sourceMappingURL=script.js.map
--- 09-ToDoApp/script.ts
+++ 09-ToDoApp/script.ts
@@ -2,15 +2,17 @@ window.addEventListener("load", function (): void {
 
     const toDoInput: HTMLInputElement = document.querySelector("#toDoInput");
     const list: HTMLUListElement = document.querySelector("#toDos");
-    interface ToDo {checked: boolean; content: string; number: number; }
+
+    interface ToDo { checked: boolean; content: string; toDoId: string; checkmarkId: string; trashId: string; }
     let allToDos: ToDo[] = [];
 
     console.log(allToDos);
 
     toDoInput.addEventListener("keydown", function (event: KeyboardEvent): void {
         if (event.keyCode == 13) {
-          
-            allToDos.push({checked: false, content: toDoInput.value, number: allToDos.length});
+
+            let length: number = allToDos.length;
+            allToDos.push({checked: false, content: toDoInput.value, toDoId: length.toString(), checkmarkId: "check" + length.toString(), trashId: "trash" + length.toString()});
             console.log(allToDos);
 
             displayList();
@@ -28,11 +30,18 @@ window.addEventListener("load", function (): void {
 
             let idToDo: Attr = document.createAttribute("id");
             let idChecked: Attr = document.createAttribute("id");
+            let idTrash: Attr = document.createAttribute("id");
             idToDo.value = i.toString();
             idChecked.value = "check" + i.toString();
+            idTrash.value = "trash" + i.toString();
 
             let checkbox: HTMLElement = document.createElement("i");
             let checked: Attr = document.createAttribute("class");
+
+            let trashIcon: HTMLElement = document.createElement("i");
+            let trash: Attr = document.createAttribute("class");
+            trash.value = "far fa-trash-alt";
+            trashIcon.setAttributeNode(trash);
 
             if (allToDos[i].checked == false) {
                 checked.value = "far fa-circle";
@@ -47,17 +56,33 @@ window.addEventListener("load", function (): void {
             newToDo.appendChild(checkbox);
             newToDo.appendChild(node);
             newToDo.setAttributeNode(idToDo);
+            newToDo.appendChild(trashIcon);
 
             list.appendChild(newToDo);
             
-            // checkbox.addEventListener("click", function (): void {checkToDo(); });
+            checkbox.addEventListener("click", function (): void {checkToDo(idChecked.value); });
         }
         toDoInput.value = "";
     }
 
-    // function checkToDo(): void {
-        
-    // }
+    function checkToDo(id: string): void {
+        for (let i: number = 0; i < allToDos.length; i++) {
+            let todo: ToDo = allToDos[i];
+            if (todo.checkmarkId == id) {
+                let check: HTMLElement = document.querySelector("#" + id);
+                if (todo.checked == false) {
+                    check.setAttribute("class", "fas fa-check-circle");
+                    todo.checked = true;
+                    console.log("heck");
+                }
+                else {
+                    check.setAttribute("class", "far fa-circle");
+                    todo.checked = false;
+                    console.log("yeah");
+                }
+            }
+        }
+    }
             
 
 
