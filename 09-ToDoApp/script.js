9502bfe0b22882779b365bda2f637e6814f98236
Am.D <ameliedelloro@gmail.com>
2021-05-20 17:19:53 +0200

    bruh

--- 09-ToDoApp/script.js
+++ 09-ToDoApp/script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
     console.log(allToDos);
     toDoInput.addEventListener("keydown", function (event) {
         if (event.keyCode == 13) {
-            allToDos.push({ checked: false, content: toDoInput.value });
+            allToDos.push({ checked: false, content: toDoInput.value, number: allToDos.length });
             console.log(allToDos);
             displayList();
         }
@@ -16,11 +16,30 @@ window.addEventListener("load", function () {
             var todo = allToDos[i];
             var newToDo = document.createElement("li");
             var node = document.createTextNode(todo.content);
+            var idToDo = document.createAttribute("id");
+            var idChecked = document.createAttribute("id");
+            idToDo.value = i.toString();
+            idChecked.value = "check" + i.toString();
+            var checkbox = document.createElement("i");
+            var checked = document.createAttribute("class");
+            if (allToDos[i].checked == false) {
+                checked.value = "far fa-circle";
+                checkbox.setAttributeNode(checked);
+            }
+            else {
+                checked.value = "fas fa-check-circle";
+                checkbox.setAttributeNode(checked);
+            }
+            checkbox.setAttributeNode(idChecked);
+            newToDo.appendChild(checkbox);
             newToDo.appendChild(node);
-            var element = document.getElementById("toDos");
-            element.appendChild(newToDo);
+            newToDo.setAttributeNode(idToDo);
+            list.appendChild(newToDo);
+            checkbox.addEventListener("click", function () { checkToDo(); });
         }
         toDoInput.value = "";
     }
+    // function checkToDo(): void {
+    // }
 });
 //# sourceMappingURL=script.js.map
--- 09-ToDoApp/script.ts
+++ 09-ToDoApp/script.ts
@@ -2,7 +2,7 @@ window.addEventListener("load", function (): void {
 
     const toDoInput: HTMLInputElement = document.querySelector("#toDoInput");
     const list: HTMLUListElement = document.querySelector("#toDos");
-    interface ToDo {checked: boolean; content: string; }
+    interface ToDo {checked: boolean; content: string; number: number; }
     let allToDos: ToDo[] = [];
 
     console.log(allToDos);
@@ -10,7 +10,7 @@ window.addEventListener("load", function (): void {
     toDoInput.addEventListener("keydown", function (event: KeyboardEvent): void {
         if (event.keyCode == 13) {
           
-            allToDos.push({checked: false, content: toDoInput.value});
+            allToDos.push({checked: false, content: toDoInput.value, number: allToDos.length});
             console.log(allToDos);
 
             displayList();
@@ -23,18 +23,44 @@ window.addEventListener("load", function (): void {
         for (let i: number = 0; i < allToDos.length; i++) {
             let todo: ToDo = allToDos[i];
 
-            let newToDo: HTMLElement = document.createElement("li");
+            let newToDo: HTMLLIElement = document.createElement("li");
             let node: Node = document.createTextNode(todo.content);
-            newToDo.appendChild(node);
 
-            let element: HTMLElement = document.getElementById("toDos");
-            element.appendChild(newToDo);
+            let idToDo: Attr = document.createAttribute("id");
+            let idChecked: Attr = document.createAttribute("id");
+            idToDo.value = i.toString();
+            idChecked.value = "check" + i.toString();
+
+            let checkbox: HTMLElement = document.createElement("i");
+            let checked: Attr = document.createAttribute("class");
+
+            if (allToDos[i].checked == false) {
+                checked.value = "far fa-circle";
+                checkbox.setAttributeNode(checked);
+            }
+            else {
+                checked.value = "fas fa-check-circle";
+                checkbox.setAttributeNode(checked);
+            }
+            
+            checkbox.setAttributeNode(idChecked);
+            newToDo.appendChild(checkbox);
+            newToDo.appendChild(node);
+            newToDo.setAttributeNode(idToDo);
+
+            list.appendChild(newToDo);
+            
+            checkbox.addEventListener("click", function (): void {checkToDo(); });
         }
         toDoInput.value = "";
     }
 
-      
+    // function checkToDo(): void {
+        
+    // }
             
+
+
 
 
 
