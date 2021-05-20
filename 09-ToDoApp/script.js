ecf2902bfe6b4565ce1053c2cce062432e1290dd
Am.D <ameliedelloro@gmail.com>
2021-05-20 14:08:25 +0200

    addToDoworks

--- 09-ToDoApp/script.js
+++ 09-ToDoApp/script.js
@@ -1,13 +1,26 @@
 window.addEventListener("load", function () {
     var toDoInput = document.querySelector("#toDoInput");
+    var list = document.querySelector("#toDos");
+    var allToDos = [];
+    console.log(allToDos);
     toDoInput.addEventListener("keydown", function (event) {
         if (event.keyCode == 13) {
-            var newToDo = document.createElement("p");
-            var content = document.createTextNode(toDoInput.value);
-            newToDo.appendChild(content);
+            allToDos.push({ checked: false, content: toDoInput.value });
+            console.log(allToDos);
+            displayList();
+        }
+    });
+    function displayList() {
+        list.innerHTML = "";
+        for (var i = 0; i < allToDos.length; i++) {
+            var todo = allToDos[i];
+            var newToDo = document.createElement("li");
+            var node = document.createTextNode(todo.content);
+            newToDo.appendChild(node);
             var element = document.getElementById("toDos");
             element.appendChild(newToDo);
         }
-    });
+        toDoInput.value = "";
+    }
 });
 //# sourceMappingURL=script.js.map
--- 09-ToDoApp/script.ts
+++ 09-ToDoApp/script.ts
@@ -1,22 +1,41 @@
 window.addEventListener("load", function (): void {
 
-    const toDoInput: HTMLElement = document.querySelector("#toDoInput");
+    const toDoInput: HTMLInputElement = document.querySelector("#toDoInput");
+    const list: HTMLUListElement = document.querySelector("#toDos");
+    interface ToDo {checked: boolean; content: string; }
+    let allToDos: ToDo[] = [];
+
+    console.log(allToDos);
 
     toDoInput.addEventListener("keydown", function (event: KeyboardEvent): void {
         if (event.keyCode == 13) {
-            
-            let newToDo: HTMLElement = document.createElement("p");
-            let content: Node = document.createTextNode(toDoInput.value);
-            newToDo.appendChild(content);
+          
+            allToDos.push({checked: false, content: toDoInput.value});
+            console.log(allToDos);
+
+            displayList();
+
+        }
+    });
+        
+    function displayList(): void {
+        list.innerHTML = "";
+        for (let i: number = 0; i < allToDos.length; i++) {
+            let todo: ToDo = allToDos[i];
+
+            let newToDo: HTMLElement = document.createElement("li");
+            let node: Node = document.createTextNode(todo.content);
+            newToDo.appendChild(node);
 
             let element: HTMLElement = document.getElementById("toDos");
             element.appendChild(newToDo);
         }
-    });
-        
+        toDoInput.value = "";
+    }
 
+      
+            
 
-    
 
 
 
